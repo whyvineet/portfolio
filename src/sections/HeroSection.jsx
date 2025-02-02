16fc8552247f90b4ce364677008fe98881f8501c
whyvineet <vineetkumar17112004@gmail.com>
2025-02-02 12:13:30 +0530

    update background image format from JPG to WEBP for improved performance

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   return (
     <section
       id="hero"
-      className="relative h-screen flex items-center bg-[url('/assets/profile/background.jpg')] bg-cover bg-center"
+      className="relative h-screen flex items-center bg-[url('/assets/profile/background.webp')] bg-cover bg-center"
     >
       <div className="absolute inset-0 bg-gradient-to-t from-black/100 via-black/60 to-black/20" />
       <div className="relative h-full container mx-auto px-6 flex flex-col justify-center items-center text-center">
